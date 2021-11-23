1b0f6cb08e21894717269e82716effa93ee1c952
vishalcreatiosoft <vishal.srivastava@creatiosoft.com>
2021-11-23 18:06:15 +0530

    save

--- chat-app/client/script.js
+++ chat-app/client/script.js
@@ -1,8 +1,8 @@
 // console.log('script file')
-
 import { io } from "socket.io-client";
 const socket = io("http://localhost:8000");
 
+//getting all responses into variables 
 const messageInput = document.getElementById('msg-input');
 const form = document.getElementById('form');
 const chatBox = document.querySelector('.chat-box');
@@ -10,42 +10,81 @@ const roomInput = document.getElementById('room-input');
 const roomButton = document.getElementById('roombtn');
 
 
+
+let newName;
+let name;
+
+function newUser(name){
+    socket.emit('new-user',name);
+    const userName = document.createElement('h1');
+    userName.textContent = name;
+    document.querySelector('#user-name').innerHTML = name;
+}
+
 socket.on('connect', ()=>{
     console.log('client connected ');
 })
 
-let name = prompt('Enter your name');
-if(name == ""){
+name = prompt('Enter your name');
+while(name == ""){
     alert('Please Enter your name')
-    let name = prompt('Enter your name');
-
-
-}else{
-    socket.emit('new-user',name);
+    name = prompt('Enter your name');
 }
-const userName = document.createElement('h1');
-userName.textContent = name;
-document.querySelector('#user-name').append(name);
 
-const displayMessage = (msg, position)=>{
+newUser(name);
+
+socket.on('userAlreadyExists',name=>{
+    
+    alert(`${name} is already online`);
+    newName = prompt('Try another name');
+    while(newName == name){
+        newName = prompt('Try another name');
+    }
+    newUser(newName); 
+})
+
+
+const displayMessage = (msg, position, sentTime, time,)=>{
+
     const div = document.createElement('div');
     div.innerText = msg;
     div.classList.add('msg');
     div.classList.add(position);
     chatBox.append(div);
+
+    const showTime = document.createElement('h6');
+    showTime.innerText = sentTime;
+    showTime.classList.add(time);
+    chatBox.appendChild(showTime);
+    
+
+}
+
+function addZero(num){
+    return num < 10 ? `0${num}` : num ;
 }
 
 socket.on('user-joined', name => {
-    console.log(name)
-    displayMessage(`${name} joined`, 'left');
+    //console.log(name)
+    const currentTime = new Date();    
+    const hours = currentTime.getHours() > 12 ? currentTime.getHours()-12 : currentTime.getHours();
+    const minutes = addZero(currentTime.getMinutes());
+    const sentTime = `${hours}:${minutes}`;
+    displayMessage(`${name} joined`, 'left', sentTime, 'show-time-left');
 })
 
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
     const message = messageInput.value;
     const room = roomInput.value;
-    displayMessage(`You : ${message}`,'right');
-    socket.emit('send', message,room);
+    
+    const currentTime = new Date();    
+    const hours = currentTime.getHours() > 12 ? currentTime.getHours()-12 : currentTime.getHours();
+    const minutes = addZero(currentTime.getMinutes());
+    const sentTime = `${hours}:${minutes}`;
+
+    displayMessage(`You : ${message}`,'right', sentTime, 'show-time-right');
+    socket.emit('send', message,room,sentTime);
     messageInput.value = "";
     roomInput.value="";
 
@@ -60,9 +99,18 @@ roomButton.addEventListener('click',()=>{
 })
 
 socket.on('receive', data => {
-    displayMessage(`${data.name} : ${data.message}`, 'left');
+
+    displayMessage(`${data.name} : ${data.message}`, 'left', data.time, 'show-time-left');
 })
 
 socket.on('left', name => {
-    displayMessage(`${name} left chatting`,'left');
+    displayMessage(`${name} left chatting`,'left', 'show-time-left');
+})
+
+socket.on('oldMessages', data => {
+    displayMessage(`${data.from} : ${data.message}`, 'left', data.time,'show-time-left');
+})
+
+socket.on('myMessages', data => {
+    displayMessage(`You : ${data.message}`,'right', data.time,'show-time-right');
 })
--- chat-app/server/server.js
+++ chat-app/server/server.js
@@ -4,34 +4,86 @@ const io=require("socket.io")(8000,{
     }
 })
 
+const redis = require('redis');
+const client = redis.createClient();
+
 const users = {};
+
+function getMessages(socket,name){
+    client.lrange('messages','0','-1',(error, data)=>{
+        if(error){
+            throw error;
+        }
+        if (data != null){
+            data.map(x => {
+                const usernameMessage = x.split(' : ');
+                const redisUserame = usernameMessage[0];
+                const redisMessage = usernameMessage[1];
+                const redisSentTime = usernameMessage[2];
+                //console.log(name);
+                if(redisUserame == name){
+                    socket.emit('myMessages',{message : redisMessage, time : redisSentTime});
+                }else{
+                    socket.emit('oldMessages',{from : redisUserame, message : redisMessage, time : redisSentTime});
+                }
+
+               // socket.emit('oldMessages',{from : redisUserame, message : redisMessage});
+                
+            }) 
+        }
+    })
+}
+
+function addZero(num){
+    return num < 10 ? `0${num}` : num ;
+}
+
 
 io.on("connection",socket =>{
     console.log(socket.id);
-
+    
     socket.on('new-user', name => {
-        //console.log(name);
-        users[socket.id] = name;
-        socket.broadcast.emit('user-joined',name);
+                         
+        let flag = true;           
+        for(const username in users){
+            if(users[username] == name){
+                socket.emit('userAlreadyExists',name);
+                flag = false;
+            }
+        }
+        if(flag == true)
+        {   users[socket.id] = name;
+            console.log(users[socket.id]);
+            socket.broadcast.emit('user-joined',name);
+            getMessages(socket,users[socket.id]);
+        }
+        
     })    
 
-    socket.on('send', (message,room) =>{
+    socket.on('send', (message,room,sentTime) => {
         // console.log(message)
         // console.log(room)
         if(room == ""){
-            socket.broadcast.emit('receive', {message : message, name : users[socket.id]})
+            socket.broadcast.emit('receive', {message : message, name : users[socket.id], time : sentTime })
+            client.rpush(`messages`,`${users[socket.id]} : ${message} : ${sentTime.toString()} `)
+
         }else{
-            socket.to(room).emit('receive', {message : message, name : users[socket.id]})
+            socket.to(room).emit('receive', {message : message, name : users[socket.id], time : sentTime})
+            console.log(room);
         }
         
     })
 
-    socket.on('join-room',(room, cb)=>{
+    socket.on('join-room',(room)=>{
         socket.join(room);
-        //cb(`Joined ${room}`)
+        
     })
 
     socket.on("disconnect", message => {
+        const currentTime = new Date();    
+        const hours = currentTime.getHours() > 12 ? currentTime.getHours()-12 : currentTime.getHours();
+        const minutes = addZero(currentTime.getMinutes());
+        const sentTime = `${hours}:${minutes}`;
         socket.broadcast.emit('left', users[socket.id]);
         delete users[socket.id];
     });
